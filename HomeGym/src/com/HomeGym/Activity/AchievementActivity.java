a460860c7f584a512f36bbe4b46b1c49088e0fd7
JangMyounghee <zldkszltm2@hanmail.net>
2015-08-16 14:49:22 +0900

    ㅇㅇㅇㅇㅇㅇㅇ

--- HomeGym/src/com/HomeGym/Activity/AchievementActivity.java
+++ HomeGym/src/com/HomeGym/Activity/AchievementActivity.java
@@ -6,6 +6,8 @@ import java.util.GregorianCalendar;
 import java.util.List;
 
 import com.HomeGym.Bluetooth.BluetoothSetting;
+import com.HomeGym.Controller.CameraSetting;
+import com.HomeGym.Controller.SetImage;
 import com.HomeGym.DB.DBSetting;
 import com.example.homegym.R;
 import com.handstudio.android.hzgrapherlib.graphview.RadarGraphView;
@@ -16,6 +18,7 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.util.Log;
@@ -44,6 +47,9 @@ public class AchievementActivity extends Activity {
 	private RadarGraphView rgView;
 	int thisYear, thisMonth, thisDay,reMonth;
 	GregorianCalendar calendar;
+	CameraSetting cSetting;
+	Uri currImageURI;
+	String path;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +66,7 @@ public class AchievementActivity extends Activity {
 		getDBData();
 		setRadarGraph();
 		layoutGraphView.invalidate();
+		cSetting = new CameraSetting(AchievementActivity.this);
 		
          dateBtn.setOnClickListener(new OnClickListener(){
         	 @Override
@@ -186,7 +193,7 @@ public class AchievementActivity extends Activity {
 				return true;
 				
 			case R.id.action_camera:
-				//cSetting.setCameraDialog();
+				cSetting.setBeforeAfterDialog();
 				return true;
 		
 			case R.id.action_exercise:
@@ -206,5 +213,47 @@ public class AchievementActivity extends Activity {
 		}
 		
 		return super.onOptionsItemSelected(item);
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		SetImage setImage = new SetImage();
+		if(resultCode == RESULT_OK)
+		{
+			MainActivity.MainAct.finish();
+			PreviewActivity.isResume = true;
+			//카메라로찍었을때
+			Log.v("RESULT_OK??", "ㅅㅄㅂ");
+			if(requestCode == CameraSetting.TAKE_CAMERA) //1
+			{
+				//찍은 사진을 이미지뷰에 보여줌
+				Log.v("들어오니", "ㅅㅄㅂ");
+				sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://"+ CameraSetting.tempPicturePath)));
+				Intent intentToMain = new Intent(this, MainActivity.class);
+				intentToMain.putExtra("cameraSetting","camera");
+				startActivity(intentToMain);
+				finish();
+			}
+				
+			//앨범에서 가져올 때
+			else if(requestCode==CameraSetting.TAKE_GALLERY)//2
+			{
+				
+				currImageURI=data.getData();
+				path = cSetting.getRealPathFromURI(currImageURI);
+				CameraSetting.tempPicturePath = path;
+				//이미지뷰에 보여줌
+				Intent intentToMain = new Intent(this, MainActivity.class);
+				intentToMain.putExtra("cameraSetting","gallery");
+				startActivity(intentToMain);
+				finish();
+				//setImage.setAlbumImageBackground(CameraSetting.tempPicturePath, iv);
+				}
+			}
+		else
+		{
+			Log.e("camera return error","에러");
+			return;
+		}
 	}
 }
--- HomeGym/src/com/HomeGym/Controller/CameraSetting.java
+++ HomeGym/src/com/HomeGym/Controller/CameraSetting.java
@@ -19,15 +19,15 @@ import android.util.Log;
 
 public class CameraSetting {
 	
-	private static final int TAKE_GALLERY = 2222;
-	private static final int TAKE_CAMERA = 1111;
+	public static final int TAKE_GALLERY = 2222;
+	public static final int TAKE_CAMERA = 1111;
 	Context cameraContext;
-	public static int flag;
+	public static int flag, baflag;
 	public String  Folder, Name;
-	public static String tempPicturePath;
+	public static String tempPicturePath, btempPicturePath, atempPicturePath; //b는 before이미지 경로, a는 after이미지경로
 	public String path;
 	CameraSetting cSetting;
-	Bitmap profileBitmap;
+	Bitmap profileBitmap; 
 	public String name = "HomeGym/";
 	File file;
 	Intent intent;
@@ -36,6 +36,30 @@ public class CameraSetting {
 	public CameraSetting(Context applicationContext){
 		// TODO Auto-generated constructor stub
 		 cameraContext= applicationContext;					
+	}
+	
+	public void setBeforeAfterDialog(){
+		Log.v("들어오냐", "setBeforeAfterDialog");
+		AlertDialog.Builder builder = new AlertDialog.Builder(cameraContext);     // 여기서 this는 Activity의 this
+		// 여기서 부터는 알림창의 속성 설정
+		builder.setTitle("변경할 항목을 선택하세요")        // 제목 설정
+		.setCancelable(true)        // 뒤로 버튼 클릭시 취소 가능 설정
+		.setPositiveButton("Before", new DialogInterface.OnClickListener(){      
+			public void onClick(DialogInterface dialog, int whichButton){
+				baflag = 1;   
+				setCameraDialog();
+			}
+		})
+		.setNegativeButton("After", new DialogInterface.OnClickListener(){      
+			public void onClick(DialogInterface dialog, int whichButton){
+				baflag = 2;
+				setCameraDialog();
+			}
+		});
+
+		AlertDialog dialog = builder.create();    // 알림창 객체 생성
+
+		dialog.show();    // 알림창 띄우기
 	}
 	
 	public void setCameraDialog(){
@@ -82,22 +106,27 @@ public class CameraSetting {
 	
 	public void selectCamera(){
 		/*----카메라 선택----*/
-		if(flag == 1){ 
-			Intent intent1 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-			Camera camera = Camera.open();;
-            camera.release();
-			/*----사진저장경로 바꾸기----*/	
-			Name=slSetting.createName(System.currentTimeMillis());
-			CameraSetting.tempPicturePath = Folder+Name;
-			File DirPath =new File(Environment.getExternalStorageDirectory()+"/HomeGym/");
-			if(!DirPath.exists()) DirPath.mkdirs();
-			file = new File(DirPath, Name);
-			CameraSetting.tempPicturePath= file.getAbsolutePath(); 
-            Log.v("파일경로", CameraSetting.tempPicturePath);
-			intent1.putExtra( MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-			((Activity)cameraContext).startActivityForResult(intent1, TAKE_CAMERA);
-			
-		}
+			if(flag == 1){ 
+				Intent intent1 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				Camera camera = Camera.open();;
+				camera.release();
+				/*----사진저장경로 바꾸기----*/	
+				Name=slSetting.createName(System.currentTimeMillis());
+				tempPicturePath = Folder+Name;
+				File DirPath =new File(Environment.getExternalStorageDirectory()+"/HomeGym/");
+				if(!DirPath.exists()) DirPath.mkdirs();
+				file = new File(DirPath, Name);
+				tempPicturePath= file.getAbsolutePath();
+				
+				if(baflag==1){ //before변경
+					btempPicturePath = tempPicturePath;
+				}else atempPicturePath = tempPicturePath;
+				
+				Log.v("파일경로", tempPicturePath);
+				intent1.putExtra( MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+				((Activity)cameraContext).startActivityForResult(intent1, TAKE_CAMERA);
+				
+			}
 		/*----갤러리 선택----*/
 		else if(CameraSetting.flag == 2){ 
 			Intent intent2 = new Intent();
